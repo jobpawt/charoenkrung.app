d03fa71bf4cf3aa3190a0f2ce9e74228b08477cd
jobnovember <pawat-ka@rmutp.ac.th>
2021-02-14 18:08:34 +0700

    initialize file

---
+++ android/app/src/main/kotlin/com/example/charoenkrung_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.charoenkrung_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/config.dart
@@ -0,0 +1,4 @@
+class Config {
+ static const String DATABASE = "http://10.0.2.2:3000";
+ static const String IMAGE = "http://10.0.2.2:8040";
+}
